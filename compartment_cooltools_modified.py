48ee11449a17ee3a7405d12376824d70a047fd8d
Milad Mokhtaridoost <67690973+MiladMokhtaridoost@users.noreply.github.com>
2025-02-24 14:50:32 -0500

    Update compartment_cooltools_modified.py

--- compartment_cooltools_modified.py
+++ compartment_cooltools_modified.py
@@ -1,34 +1,24 @@
 ###############################################
-## cooltools modified my Milad Mokhtaridoost (milad.mokhtaridoost@sickkids.ca)
+## cooltools modified my Milad Mokhtaridoost 
 ##############################################
 
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import pandas as pd
 import os, subprocess
-
 import cooler
 import cooltools.lib.plotting
+import cooltools
+import sys
+import bioframe
 
 from packaging import version
 if version.parse(cooltools.__version__) < version.parse('0.5.1'):
     raise AssertionError("tutorials rely on cooltools version 0.5.1 or higher,"+
                          "please check your cooltools version and update to the latest")
 
-import cooltools
+input = sys.argv[1] ##### pathway to .cool data
+prefix = sys.argv[2] ##### dataset name 
+resolution = sys.argv[3] ##### resolution of HiC data
 
-import sys
-input = sys.argv[1]
-prefix = sys.argv[2]
-resolution = sys.argv[3]
-
-#input = 1000000
-#prefix = "Pancreas_PatientF_Schmitt"
-#resolution = "Pancreas_PatientF_Schmitt"
-
-clr = cooler.Cooler(f"/hpf/largeprojects/pmaass/3D-flow/normalized_data_4DNuc_pipeline/human/{input}/{prefix}.pairs.res{resolution}.cool")
-#clr = cooler.Cooler(f"Z:/3D-flow/normalized_data_4DNuc_pipeline/human/{input}/{prefix}.pairs.res{resolution}.cool")
-
+clr = cooler.Cooler(f"{input}/{prefix}.pairs.res{resolution}.cool")
 
 ## fasta sequence is required for calculating binned profile of GC conent
 if not os.path.isfile('./hg38.fa'):
@@ -36,7 +26,6 @@ if not os.path.isfile('./hg38.fa'):
     subprocess.call('wget https://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.gz', shell=True)
     subprocess.call('gunzip hg38.fa.gz', shell=True)
 
-import bioframe
 bins = clr.bins()[:]
 hg38_genome = bioframe.load_fasta('./hg38.fa');
 ## note the next command may require installing pysam
@@ -50,9 +39,7 @@ cis_eigs = cooltools.eigs_cis(
                         n_eigs=3,
                         )
 
-# cis_eigs[0] returns eigenvalues, here we focus on eigenvectors
 eigenvector_track = cis_eigs[1][['chrom','start','end','E1']]
 
-#eigenvector_track.to_csv(f"{input}_compartments{resolution}.csv")
-eigenvector_track.to_csv(f"/hpf/largeprojects/pmaass/3D-flow/normalized_data_4DNuc_pipeline/human/{input}/{input}_compartments{resolution}.csv", encoding='utf-8', index=False)
-#eigenvector_track.to_csv(f"Z:/3D-flow/normalized_data_4DNuc_pipeline/human/{input}/{input}_compartments{resolution}.csv", encoding='utf-8', index=False)
+### save as csv file
+eigenvector_track.to_csv(f"{input}/{prefix}_compartments{resolution}.csv", encoding='utf-8', index=False)
